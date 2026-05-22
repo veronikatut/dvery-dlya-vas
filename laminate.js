0615c0cd00da94702c1be457c00f038ebf738a41
veronikatut <veronikasolnce7@gmail.com>
2026-05-22 21:45:08 +0200

    Add files via upload

---
+++ laminate.js
@@ -0,0 +1,12 @@
+const laminateHTML = `
+    <div class="product-card">
+        <img src="door1.jpg" alt="Ламинат" class="product-card__img">
+        <div class="product-card__body">
+            <h3 class="product-card__title">ТОВАР<br>В РАЗРАБОТКЕ</h3>
+            <p class="product-card__short-desc">Скоро здесь появятся товары</p>
+            <div class="product-card__footer">
+                <span class="product-card__price">— ₸</span>
+            </div>
+        </div>
+    </div>
+`;
